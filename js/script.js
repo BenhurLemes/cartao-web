230a11d3b41baeb040e66bb36d4a854ab678d4a2
unknown <benhurlemes1@gmail.com>
2023-10-25 17:06:10 -0300

    cartao2

---
+++ js/script.js
@@ -0,0 +1,79 @@
+// Loader
+window.addEventListener('load', function(){
+    this.document.querySelector('.preloader').classList.add('opacity-0');
+    setTimeout(function(){
+        document.querySelector('.preloader').style.display='none';
+    },1000);
+});
+// Fim do Loader
+
+// Cartão
+
+const form = document.querySelector("#credit-card");
+
+const cardNumber = document.querySelector("#card-number");
+const cardHolder = document.querySelector("#name-text");
+const cardExpiration = document.querySelector("#valid-thru-text");
+const cardCVC = document.querySelector("#cvc-text");
+
+const cardNumberText = document.querySelector(".number-v1");
+const cardHolderText = document.querySelector(".name-v1");
+const cardExpirationText = document.querySelector(".expiration-v1");
+const cardCVCText = document.querySelector(".cvc-v1");
+
+cardNumber.addEventListener("keyup", (e) => {
+    if(!e.target.value) {
+        cardNumberText.innerText = "1234 5678 9023 4567";
+    } else {
+        const valuesOfInput = e.target.value.replaceAll(" ","");
+        if (e.target.value.length > 14) {
+            e.target.value = valuesOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+            cardNumberText.innerHTML = valuesOfInput.valuesOfInput.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+        } else if (e.target.value.length > 9) {
+            e.target.value = valuesOfInput.valuesOfInput.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+            cardNumberText.innerHTML = valuesOfInput.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+        } else if (e.target.value.length > 4) {
+            e.target.value = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+            cardNumberText.innerHTML = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+        } else {
+            cardNumberText.innerHTML = valuesOfInput;
+        }
+    }
+});
+
+cardHolder.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardHolderText.innerHTML = "Lorem Ipsum";
+    } else {
+        cardCVCText.innerHTML = e.target.value.toUpperCase();
+    }
+});
+
+cardExpiration.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardExpirationText.innerHTML = "01/11"
+    } else {
+        const valuesOfInput = e.target.value.replace("/", "");
+
+        if(e.target.value.length > 2){
+            e.target.value = valuesOfInput.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
+            cardExpirationText.innerHTML = valuesOfInput.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
+        } else {
+            cardExpirationText.innerHTML = valuesOfInput;
+        }
+    }
+});
+
+cardCVC.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardCVCText.innerHTML = "123";
+    } else{
+        cardCVCText.innerHTML = e.target.value;
+    }
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    alert("Credit Card Added!");
+})
---
+++ readme.md
@@ -0,0 +1,47 @@
+# Cartão Nubank com formulário
+
+Um cartão que lembre o cartão de crédito da Nubank, com um formulário para aplicar as entradas no cartão, poder ver as informações escritas no cartão que você fez.
+
+## Ideias
+
+- Fundo bonito
+- Dizer quem foi o desenvolvedor 
+- O visual do cartão
+    - Frente
+    - Atrás
+- Formulário para escrever o que precisa mudar o que está escrito no cartão
+
+CSS FLEX
+
+Card flex coluna
+    div linha (coluna)
+        div dados (row)
+    div linha (coluna)
+        div dados (row)
+    div linha (coluna)
+        div dados (row)
+
+### Ferramentas
+
+* HTML
+* CSS
+* Javascript
+
+#### itens para guardar
+
+* HTML
+
+    <!-- <div class="card-holder">
+        <label class="label-name">Nome</label>
+        <div class="name-v1">Lorem Ipsum</div>
+    </div> -->
+
+* CSS
+
+    <!-- .card-holder {
+        color: whitesmoke;
+    }
+
+    .card-holder label{
+        font-size: .7rem;
+    } -->
